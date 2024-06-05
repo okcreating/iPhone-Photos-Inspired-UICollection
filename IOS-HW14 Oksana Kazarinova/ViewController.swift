3ea3ccb5057cd36ed2f39cf8e5a2adc17e678774
okcreating <oksibearus@gmail.com>
2024-06-05 11:00:04 +0100

    Initial Commit

---
+++ IOS-HW14 Oksana Kazarinova/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS-HW14 Oksana Kazarinova
+//
+//  Created by Oksana Kazarinova on 05/06/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
