cb0c7cceb1378019b18a4a9714b09a59b72158a5
okcreating <oksibearus@gmail.com>
2024-06-06 15:53:38 +0100

    Ready TabBar

---
+++ IOS-HW14 Oksana Kazarinova/Sources/AlbumViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AlbumViewController.swift
+//  IOS-HW14 Oksana Kazarinova
+//
+//  Created by Oksana Kazarinova on 06/06/2024.
+//
+
+import UIKit
+
+class AlbumViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+
+}
