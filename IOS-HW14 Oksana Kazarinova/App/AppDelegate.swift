15703fca3256fae144bc26869bf2764af9861f41
okcreating <oksibearus@gmail.com>
2024-06-06 11:13:49 +0100

    Project adjusted

---
+++ IOS-HW14 Oksana Kazarinova/App/AppDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  AppDelegate.swift
+//  IOS-HW14 Oksana Kazarinova
+//
+//  Created by Oksana Kazarinova on 05/06/2024.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate { }
+
