b8aba590d0216641882762d35f2574ad31d6f093
okcreating <oksibearus@gmail.com>
2024-06-07 14:05:40 +0100

    methods and headers created

---
+++ IOS-HW14 Oksana Kazarinova/Items/SectionHeaderWithSeeAllButton.swift
@@ -0,0 +1,75 @@
+//
+//  SectionHeaderWithSeeAllButton.swift
+//  IOS-HW14 Oksana Kazarinova
+//
+//  Created by Oksana Kazarinova on 07/06/2024.
+//
+
+import UIKit
+
+class SectionHeaderWithSeeAllButton: UICollectionReusableView {
+    static let identifier = "HeaderWithButton"
+
+    // MARK: - Outlets
+
+    lazy var title: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 25, weight: .semibold)
+        return label
+    }()
+
+    lazy var seeAllButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .clear
+        button.setTitle("See All", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+       // button.frame = CGRect(x: 303, y: 14, width: 62, height: 12)
+        button.addTarget(self, action: #selector(showDetail), for: .touchUpInside)
+        return button
+    }()
+
+    lazy var stackView: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .horizontal
+        return stack
+    }()
+
+    // MARK: - Initializers
+
+    override init(frame: CGRect) {
+        super .init(frame: frame)
+        clipsToBounds = true
+        setupHierarchy()
+        setupLayout()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: Setups
+
+    private func setupHierarchy() {
+        addSubview(stackView)
+        stackView.addSubview(title)
+        stackView.addSubview(seeAllButton)
+    }
+
+    private func setupLayout() {
+        title.snp.makeConstraints { make in
+            make.leading.equalTo(stackView).offset(5)
+            make.top.bottom.equalTo(stackView)
+        }
+        seeAllButton.snp.makeConstraints { make in
+            make.right.equalTo(stackView.snp_rightMargin).offset(-5)
+            make.top.bottom.equalTo(stackView)
+        }
+    }
+
+    // MARK: - Actions
+
+    @objc func showDetail () {
+
+    }
+}
--- IOS-HW14 Oksana Kazarinova/Sources/AlbumViewController.swift
+++ IOS-HW14 Oksana Kazarinova/Sources/AlbumViewController.swift
@@ -9,11 +9,93 @@ import UIKit
 
 class AlbumViewController: UIViewController {
 
+    // MARK: - Outlets
+
+    private lazy var albumsCollectionView: UICollectionView = {
+        let layout = createLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(SectionsHeader.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: SectionsHeader.identifier)
+        collectionView.register(SectionHeaderWithSeeAllButton.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: SectionHeaderWithSeeAllButton.identifier)
+
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        return collectionView
+    }()
+
+    // MARK: - Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
+        title = "Albums"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        setupHierarchy()
+        setupLayout()
     }
 
+    // MARK: - Setups
+
+    private func setupHierarchy() {
+        view.addSubview(albumsCollectionView)
+    }
+
+    private func setupLayout() {
+        albumsCollectionView.snp.makeConstraints { make in
+            make.leading.trailing.equalTo(view)
+            make.top.equalTo(navigationController?.navigationBar ?? view).offset(15)
+            make.bottom.equalTo(tabBarController?.tabBar ?? view)
+        }
+    }
+
+    // MARK: - CollectionViewLayout
+
+    private func createLayout() -> UICollectionViewCompositionalLayout {
+        return UICollectionViewCompositionalLayout { sectionIndex, _ in
+            <#code#>
+        }
+    }
+}
+
+extension AlbumViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        4
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        switch indexPath.section {
+        case 0:
+            let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: SectionHeaderWithSeeAllButton.identifier, for: indexPath) as? SectionHeaderWithSeeAllButton
+            header?.title.text = "My Albums"
+            return header ?? UICollectionReusableView()
+        case 1:
+            let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: SectionsHeader.identifier, for: indexPath) as? SectionsHeader
+            header?.title.text = "People & Places"
+            return header ?? UICollectionReusableView()
+        case 2:
+            let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: SectionsHeader.identifier, for: indexPath) as? SectionsHeader
+            header?.title.text = "Media Types"
+            return header ?? UICollectionReusableView()
+        case 3:
+            let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: SectionsHeader.identifier, for: indexPath) as? SectionsHeader
+            header?.title.text = "Utilities"
+            return header ?? UICollectionReusableView()
+        default:
+            let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: SectionsHeader.identifier, for: indexPath) as? SectionsHeader
+            header?.title.text = "Other photos"
+            return header ?? UICollectionReusableView()
+        }
+    }
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        <#code#>
+    }
 
 }
